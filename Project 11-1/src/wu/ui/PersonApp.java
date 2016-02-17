7d108edb9b82e80c6c00652b3d57fc4de5e62b01
Charles Martinez <Chuckmode@wireless-10-146-134-20.public.utexas.edu>
2016-02-16 22:15:46 -0600

    chchchchanges

--- Project 11-1/src/wu/ui/PersonApp.java
+++ Project 11-1/src/wu/ui/PersonApp.java
@@ -1,10 +1,30 @@
 package wu.ui;
-
+import java.util.Scanner;
 public class PersonApp {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Let's see");
+        System.out.println("Welcome to the Person Tester application");
+        System.out.println();
+
+        Scanner sc = new Scanner (System.in);
+        String choice1 = "c";
+        While(choice.equalsIgnoreCase("c")) {
+            System.out.print("Enter first name:  ");
+            String first = sc.nextLine();
+            System.out.print("Enter last name:  ");
+            String last = sc.nextLine();
+            System.out.print("Enter email address:  ");
+            String mail = sc.nextLine();
+            System.out.print("Customer number:  ");
+            String num = sc.nextLine();
+            System.out.println();
+            System.out.println("You entered:");
+            System.out.println("Name: " + first + last);
+            System.out.println("Email: " + mail);
+            System.out.println("Customer number: " + num);
+            System.out.println();
+
+        }
     }
 
 }
