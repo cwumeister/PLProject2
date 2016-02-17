03a22fb96807b3eb5d90799a87d88ad85ea34c0a
Connie Wu <conniewu@utexas.edu>
2016-02-16 21:09:07 -0600

    new change

---
+++ Project 11-1/src/wu/business/Employee.java
@@ -0,0 +1,10 @@
+package wu.business;
+
+public class Employee {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
---
+++ Project 11-1/src/wu/ui/Console.java
@@ -0,0 +1,10 @@
+package wu.ui;
+
+public class Console {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
