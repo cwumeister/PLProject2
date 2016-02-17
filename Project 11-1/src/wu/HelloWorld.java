695bdba060ab9820f0ebaf739393b0f0b183f1ff
Charles Martinez <Chuckmode@wireless-10-146-134-20.public.utexas.edu>
2016-02-16 20:53:30 -0600

    Revert "khgjh"
    This reverts commit a364de9c2d89b2f485d49ab79293004187788fb0.

--- Project 11-1/src/wu/HelloWorld.java
+++ Project 11-1/src/wu/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello Connie");
+        System.out.println("Hello, World");
     }
 
 }
