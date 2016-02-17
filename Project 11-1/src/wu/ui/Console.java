cbd5aefc3243868a61481fe2ec0c7504e65dd93a
Connie Wu <conniewu@utexas.edu>
2016-02-16 21:42:18 -0600

    Console from book

--- Project 11-1/src/wu/ui/Console.java
+++ Project 11-1/src/wu/ui/Console.java
@@ -1,10 +1,51 @@
 package wu.ui;
 
+import java.util.Scanner;
+
 public class Console {
 
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+   private static Scanner sc = new Scanner(System.in);
+
+    public static void displayLine(){
+        System.out.println();
     }
 
+    public static void displayLine(String s){
+        System.out.println(s);
+    }
+
+    public static String getString(String prompt){
+        System.out.print(prompt);
+        String s = sc.nextLine();
+        return s;
+    }
+
+    public static int getInt(String prompt){
+        int i = 0;
+        while (true){
+            System.out.print(prompt);
+            try{
+                i = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (NumberFormatException e){
+                System.out.println("Error! Invalid integer. Try again.");
+            }
+        }
+        return i;
+    }
+
+    public static double getDouble(String prompt){
+        double d = 0;
+        while (true){
+            System.out.print(prompt);
+            try {
+                d = Double.parseDouble(sc.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Error! Invalid decimal. Try again.");
+            }
+
+        }
+        return d;
+    }
 }
