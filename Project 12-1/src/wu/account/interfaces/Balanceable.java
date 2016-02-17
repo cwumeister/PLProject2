475d1fbb0873ded840e9e4eb17c8a35c6fd0b38c
Connie Wu <conniewu@utexas.edu>
2016-02-16 21:16:08 -0600

    last folders

---
+++ Project 12-1/src/wu/account/interfaces/Balanceable.java
@@ -0,0 +1,10 @@
+package wu.account.interfaces;
+
+public class Balanceable {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
---
+++ Project 12-1/src/wu/business/Transactions.java
@@ -0,0 +1,10 @@
+package wu.business;
+
+public class Transactions {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
