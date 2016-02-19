9877732219552d4d7dd92f50d0ccec7e2226597d
Connie Wu <conniewu@utexas.edu>
2016-02-19 12:20:55 -0600

    Project 12-1

---
+++ Project 12-1/src/wu/account/interfaces/Balanceable.java
@@ -0,0 +1,7 @@
+package wu.account.interfaces;
+
+/**
+ * Created by Connie Wu on 2/19/2016.
+ */
+public class Balanceable {
+}
---
+++ Project 12-1/src/wu/business/Transactions.java
@@ -0,0 +1,7 @@
+package wu.business;
+
+/**
+ * Created by Connie Wu on 2/19/2016.
+ */
+public class Transactions {
+}
