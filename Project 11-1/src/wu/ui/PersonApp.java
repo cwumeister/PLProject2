f58eb6ab93dc18c72ad093272e529f6b2c8ba5e8
Charles Martinez <Chuckmode@wireless-10-147-96-32.public.utexas.edu>
2016-02-18 19:47:46 -0600

    personApp

--- Project 11-1/src/wu/ui/PersonApp.java
+++ Project 11-1/src/wu/ui/PersonApp.java
@@ -1,30 +1,60 @@
 package wu.ui;
+
 import java.util.Scanner;
+
 public class PersonApp {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Person Tester application");
-        System.out.println();
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Welcome to the Person App");
+        Scanner sc = new Scanner(System.in);
+        String choice = "y";
 
-        Scanner sc = new Scanner (System.in);
-        String choice1 = "c";
-        While(choice.equalsIgnoreCase("c")) {
-            System.out.print("Enter first name:  ");
-            String first = sc.nextLine();
-            System.out.print("Enter last name:  ");
-            String last = sc.nextLine();
-            System.out.print("Enter email address:  ");
-            String mail = sc.nextLine();
-            System.out.print("Customer number:  ");
-            String num = sc.nextLine();
-            System.out.println();
-            System.out.println("You entered:");
-            System.out.println("Name: " + first + last);
-            System.out.println("Email: " + mail);
-            System.out.println("Customer number: " + num);
-            System.out.println();
+        while (choice.equalsIgnoreCase("y"))
+        {
+            System.out.print("Create customer or employee? (c/e) ");
+            String cOre = sc.nextLine();
 
+            if (cOre.equalsIgnoreCase("c"))
+            {
+                System.out.print("\nEnter first name: ");
+                String first = sc.nextLine();
+
+                System.out.print("Enter last: ");
+                String last = sc.nextLine();
+
+                System.out.print("Enter email address: ");
+                String email = sc.nextLine();
+
+                System.out.print("Customer number: ");
+                String cust = sc.nextLine();
+
+                System.out.println("\nYou entered:\nName: "+first+" "+last+"\nEmail: "+email+"\nCustomer number: "+cust);
+
+            }
+            else if (cOre.equalsIgnoreCase("e"))
+            {
+                System.out.print("\nEnter first name: ");
+                String first = sc.nextLine();
+
+                System.out.print("Enter last: ");
+                String last = sc.nextLine();
+
+                System.out.print("Enter email address: ");
+                String email = sc.nextLine();
+
+                System.out.print("Social security number: ");
+                String social = sc.nextLine();
+
+                System.out.println("\nYou entered:\nName: "+first+" "+last+"\nEmail: "+email+"\nSocial security number: "+social);
+            }
+
+
+
+            System.out.print("\nContinue? (y/n): ");
+            choice = sc.nextLine();
+            System.out.println();
         }
-    }
 
+    }
 }
