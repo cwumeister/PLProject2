403278a9cd8b2ef33b15fbf04a8bd813c7d22ca4
Connie Wu <conniewu@utexas.edu>
2016-02-19 12:37:19 -0600

    accountapp

--- Project 12-1/src/wu/ui/AccountApp.java
+++ Project 12-1/src/wu/ui/AccountApp.java
@@ -1,11 +1,37 @@
 package wu.ui;
-
+import java.util.Scanner;
 /**
  * Created by Connie Wu on 2/19/2016.
  */
 public class AccountApp {
 
     public static void main (String[] args) {
-        System.out.println("Hello team");
+        System.out.println("Welcome to the Account Calculator");
+        System.out.println();
+
+        System.out.println("Starting Balance");
+        System.out.println("Checking: ");
+
+        Scanner sc = new Scanner(System.in);
+        String choice = "y";
+
+        while (choice.equalsIgnoreCase("y")){
+
+            System.out.print("\nWithdraw or deposit? (w/d): ");
+            String wOd = sc.nextLine();
+
+            if (wOd.equalsIgnoreCase("w")){
+                System.out.print("Withdraw");
+            }
+            else if (wOd.equalsIgnoreCase("d")){
+                System.out.println("Desposit");
+            }
+
+            System.out.print("\nContinue? (y/n): ");
+            choice = sc.nextLine();
+            System.out.println();
+
+        }
+
     }
 }
