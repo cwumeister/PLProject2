4816d0da769fab392621916a07bdf7fc80390ba0
Happy Situ <happysitu@utexas.edu>
2016-02-19 12:35:05 -0600

    methods

--- Project 12-1/src/wu/account/interfaces/Balanceable.java
+++ Project 12-1/src/wu/account/interfaces/Balanceable.java
@@ -4,4 +4,11 @@ package wu.account.interfaces;
  * Created by Connie Wu on 2/19/2016.
  */
 public class Balanceable {
+    double getBalance() {
+        return 0; //not supposed to return 0
+    }
+    void setBalance(double amount) {
+
+    }
+
 }
--- Project 12-1/src/wu/account/interfaces/Depositable.java
+++ Project 12-1/src/wu/account/interfaces/Depositable.java
@@ -4,4 +4,5 @@ package wu.account.interfaces;
  * Created by Connie Wu on 2/19/2016.
  */
 public class Depositable {
+    void deposit(double amount) {}
 }
