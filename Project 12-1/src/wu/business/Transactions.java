8dc5caa817cb1b09558059f472e88798a009de7d
Happy Situ <happysitu@utexas.edu>
2016-02-19 13:09:08 -0600

    transactions works

--- Project 12-1/src/wu/business/Transactions.java
+++ Project 12-1/src/wu/business/Transactions.java
@@ -1,7 +1,19 @@
 package wu.business;
 
+import wu.account.interfaces.Depositable;
+import wu.account.interfaces.Withdrawable;
+
 /**
  * Created by Connie Wu on 2/19/2016.
  */
 public class Transactions {
+
+    public static void deposit(Depositable account, double amount) {
+        account.deposit(amount);
+    }
+    public static void withdraw(Withdrawable account, double amount) {
+        account.withdraw(amount);
+    }
+
+
 }
