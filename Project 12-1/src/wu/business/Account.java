86a2a3197cc059bc3c08c37e4f644b03a7ade1c0
Charles Martinez <Chuckmode@wireless-10-145-224-50.public.utexas.edu>
2016-02-19 13:26:44 -0600

    Account

--- Project 12-1/src/wu/business/Account.java
+++ Project 12-1/src/wu/business/Account.java
@@ -1,7 +1,30 @@
 package wu.business;
+import java.text.NumberFormat;
 
 /**
  * Created by Connie Wu on 2/19/2016.
  */
 public class Account {
+    private double deposit;
+    private double withdraw;
+    private double balance;
+
+    public Account(){}
+
+    public void setDeposit(double deposit){
+        this.deposit = deposit;}
+    public double getDeposit(){return deposit;}
+
+    public void setWithdraw(double withdraw){
+        this.withdraw = withdraw;}
+    public double getWithdraw(){return withdraw;}
+
+    public void setBalance(double balanace){
+        this.balance = balance;}
+    public double getBalance(){return balance;}
+
+    public String getBalanceFormatted(){
+        NumberFormat balance = NumberFormat.getCurrencyInstance();
+        return balance.format(balance);
+    }
 }
